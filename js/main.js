316b816955b218378c71ff0d3d82a83e051bc32d
Daniel Yin <danielyin0920@hotmail.com>
2017-06-07 00:48:59 +0100

    created game logic (2)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 console.log("Up and running!")
-
+/*
 var cardOne = "queen";
 console.log("User flipped " + cardOne)
 var cardTwo = "queen";
@@ -8,3 +8,22 @@ var cardThree = "king";
 console.log("User flipped " + cardThree)
 var cardFour = "king";
 console.log("User flipped " + cardFour)
+*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardsInPlay[0]);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardsInPlay[1]);
+if (cardsInPlay.length > 1) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, please try again.");
+	}
+} else {
+	alert("Please pick another card!");
+}
