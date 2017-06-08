07a5b1ef370a9c5363dd2b95fa61b0964f53c9d8
Daniel Yin <danielyin0920@hotmail.com>
2017-06-08 20:34:34 +0100

    Customization Completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 console.log("Up and running!")
+
 var cards = [
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
 	{
 		rank: "queen",
 		suit: "hearts",
@@ -14,31 +20,12 @@ var cards = [
 		rank: "king",
 		suit: "hearts",
 		cardImage: "images/king-of-hearts.png"
-	},
-	{
-		rank: "king",
-		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
 	}
 ];
-var cardsInPlay = [];
-function flipCards(){
-	var cardId = this.getAttribute('data-id');
-	this.setAttribute('src', cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
-	if (cardsInPlay.length > 1) {		
-		checkForMatch();
-	} else {}
-}
-function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, please try again.");
-	}
-}
+
 function createBoard(){
+	$('button').hide();
+	$('#newGame').hide();
 	for(var i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
@@ -46,5 +33,39 @@ function createBoard(){
 		cardElement.addEventListener('click', flipCards);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
+	$(document.getElementsByTagName('img')).hide();
+	$(document.getElementsByTagName('img')).fadeIn(1000);
 }
-createBoard();
+
+var cardsInPlay = [];
+
+function flipCards(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	$(this).animate({width: "200px"});
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+	setTimeout(function(){
+		if (cardsInPlay.length > 1) {		
+			checkForMatch();
+			cardsInPlay = [];
+		} else {}
+	}, 500);
+}
+
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, please try again.");
+		window.location.reload();
+	}
+}
+
+$('.instructions').hide();
+$('#instructionsButton').on('click', function(){
+	$('.instructions').slideToggle(400);
+});
+
+document.getElementById('newGame').addEventListener('click', createBoard);
+document.getElementById('newBoard').addEventListener('click', createBoard);
